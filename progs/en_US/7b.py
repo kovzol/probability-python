a1b2a6f91698869cb8bb376a2760797810323bf4
Zoltán Kovács <zoltan@geogebra.org>
2021-02-19 17:34:06 +0100

    Add the remaining sections

--- progs/en_US/7b.py
+++ progs/en_US/7b.py
@@ -1,11 +1,11 @@
 import itertools
 import matplotlib.pyplot as p
-max_tosses = 4
-for W in range(1, max_tosses + 1):
-  Ω = set(itertools.product({1, 2, 3, 4, 5, 6}, repeat=W))
-  sums = [0] * (max_tosses * 6 + 1)
+max_dice = 4
+for d in range(1, max_dice + 1):
+  Ω = set(itertools.product({1, 2, 3, 4, 5, 6}, repeat=d))
+  sums = [0] * (max_dice * 6 + 1)
   for ω in Ω:
     sums[sum(ω)] += 1
   probabilities = [h / len(Ω) for h in sums]
-  p.plot(range(1, max_tosses * 6 + 1), probabilities[1:])
+  p.plot(range(1, max_dice * 6 + 1), probabilities[1:])
 p.show()
